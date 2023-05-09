693b81db1791d94366df650d1b5f63c3a8a4e20c
Alberto-Jordan <alberto.jordan408@gmail.com>
2023-05-09 06:04:40 -0600

    spinner2

---
+++ spinner2.js
@@ -0,0 +1,16 @@
+process.stdout.write('hello from spinner1.js... \rheyyy\n');
+
+const spinnerChars = ['|', '/', '-', '\\', '|', '/', '-', '\\', '|'];
+
+let delay = 100;
+
+for (const char of spinnerChars) {
+  setTimeout(() => {
+    process.stdout.write(`\r${char}   `);
+  }, delay);
+  delay += 200;
+}
+
+setTimeout(() => {
+  process.stdout.write('\n');
+}, delay);
